76f6fbb800a5a252d96031136b3409b272fc9898
manik-kyau <you@example.commaniksarkar.kyau.cse>
2024-06-11 19:44:08 +0600

    create Admin Private Route

--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -1,24 +1,26 @@
-import PropTypes from 'prop-types';
 import { Navigate, useLocation } from "react-router-dom";
+import useAdmin from "../Hooks/useAdmin";
 import useAuth from "../Hooks/useAuth";
+import PropTypes from 'prop-types';
 
-const PrivateRoutes = ({ children }) => {
+const AdminRoute = ({ children }) => {
+    const [isAdmin, isAdminLoading] = useAdmin();
     const { user, loading } = useAuth();
     const location = useLocation();
 
-    if (loading) {
+    if (loading || isAdminLoading) {
         return <div className="flex justify-center items-center h-full">
             <div className="w-16 h-16 border-4 border-dashed border-green-600 rounded-full animate-spin dark:border-violet-600"></div>
         </div>
     }
 
-    if (user) {
+    if (user && isAdmin) {
         return children;
     }
     return <Navigate to='/login' state={{ from: location }} replace></Navigate >
 };
-PrivateRoutes.propTypes = {
+AdminRoute.propTypes = {
     children: PropTypes.object.isRequired,
 }
 
-export default PrivateRoutes;
+export default AdminRoute;
